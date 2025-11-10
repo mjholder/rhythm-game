40466b0909aa7d98fb5bcb8707964fe235b01b34
Matt Holder <mjholder96@gmail.com>
2025-11-10 17:44:09 -0500

    shmoovin

--- scripts/title_control.gd
+++ scripts/title_control.gd
@@ -1,10 +1,29 @@
 extends Control
 
 @export var radius: float = 100.0
-@export var offset: Vector2 = Vector2.ONE
+@export var offset: float = 0.0
+@export var speed: float = 1.0
 @export var title: Label
 @export var shadows: Array[Label] = []
 
-func _process(delta: float):
-    for i in shadows.size():
-        shadows[i].position = offset * i + Vector2(radius * cos(i * 2 * PI / shadows.size()), radius * sin(i * 2 * PI / shadows.size()))
+var angle: float = 0.0
+
+func _ready() -> void:
+	for i in shadows.size():
+		shadows[i].position = title.position + Vector2(radius * cos(offset + i * 2 * PI / shadows.size()), radius * sin(offset + i * 2 * PI / shadows.size()))
+
+func _process(delta: float) -> void:
+	# Increase the angle over time
+	angle += speed * TAU * delta  # TAU = 2 * PI
+	
+	# Compute position along the circle
+	var x = title.position.x + radius * cos(angle)
+	var y = title.position.y + radius * sin(angle)
+	
+	title.position = Vector2(x, y)
+
+	for i in shadows.size():
+		var offset_angle = angle - deg_to_rad(offset * (i + 1))
+		x = shadows[i].position.x + radius * cos(offset_angle)
+		y = shadows[i].position.y + radius * sin(offset_angle)
+		shadows[i].position = Vector2(x, y)
