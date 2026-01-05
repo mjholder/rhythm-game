b747653f344d034a02b600125ca8474b2e88c219
Matt Holder <mjholder96@gmail.com>
2026-01-04 20:28:35 -0500

    clicking works now

--- scripts/clap_level.gd
+++ scripts/clap_level.gd
@@ -2,6 +2,11 @@ extends Level
 class_name ClapLevel
 
 @export var clap_hands: AnimatedSprite2D
+@export var song_player: AudioStreamPlayer
+
+func play_song():
+	song_player.play()
+
 
 func on_action():
 	super.on_action()
@@ -9,3 +14,7 @@ func on_action():
 		clap_hands.set_frame_and_progress(1, 1.0)
 	else:
 		clap_hands.play("clap")
+		var time = song_player.get_playback_position() + AudioServer.get_time_since_last_mix()
+		# Compensate for output latency.
+		time -= AudioServer.get_output_latency()
+		print("Time is: ", time)
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,17 +1,26 @@
 extends Node2D
 class_name GameManager
 
+@export var level_parent: Node2D
 @export var current_level: Level
 
 func _ready() -> void:
-    pass
+	current_level = level_parent.get_children()[0]
 
 func select_level(level: Level) -> void:
-    pass
+	current_level.queue_free()
+	current_level = level
+	level_parent.add_child(current_level)
+	current_level.play_song()
 
 func _process(delta: float) -> void:
-    pass
+	pass
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("action"):
-        current_level.on_action()
+	if event.is_action_pressed("action"):
+		current_level.on_action()
+
+
+func _on_levelselectbutton_pressed() -> void:
+	print("levelselectbutton pressed")
+	select_level(load("res://scenes/clap_level.tscn").instantiate())
