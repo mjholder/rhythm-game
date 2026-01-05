9a6f6fa71573a778a32f969bde6e9953e187c86c
Matt Holder <mjholder96@gmail.com>
2026-01-05 17:36:54 -0500

    basic menu controls

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,6 +3,9 @@ class_name GameManager
 
 @export var level_parent: Node2D
 @export var current_level: Level
+@export var main_menu: Control
+@export var level_select: Control
+@export var pause_menu: Control
 
 func _ready() -> void:
 	current_level = level_parent.get_children()[0]
@@ -12,6 +15,7 @@ func select_level(level: Level) -> void:
 	current_level = level
 	level_parent.add_child(current_level)
 	current_level.play_song()
+	hide_all_menus()
 
 func _process(delta: float) -> void:
 	pass
@@ -20,6 +24,25 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("action"):
 		current_level.on_action()
 
+func hide_all_menus() -> void:
+	main_menu.hide()
+	level_select.hide()
+	pause_menu.hide()
+
+func show_main_menu() -> void:
+	main_menu.show()
+	level_select.hide()
+	pause_menu.hide()
+
+func show_level_select() -> void:
+	main_menu.hide()
+	level_select.show()
+	pause_menu.hide()
+
+func show_pause_menu() -> void:
+	main_menu.hide()
+	level_select.hide()
+	pause_menu.show()
 
 func _on_levelselectbutton_pressed() -> void:
 	print("levelselectbutton pressed")
